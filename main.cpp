2ea600f4ba6eb0c473e5f52b57815194debba5e8
Racheltbt20 <tabita.rachel20@gmail.com>
2025-03-20 20:04:12 +0700

    ❤🩹

--- main.cpp
+++ main.cpp
@@ -2,11 +2,29 @@
 using namespace std;
 
 int main() {
-    // variabel
-    double matriks1[2][2], matriks2[2][2], hasil[2][2] = {0}, det;
-    int i, j, k, pilih;
+    // start deklarasi variabel
+    double matriks1[2][2] = {0}, matriks2[2][2] = {0}, invers[2][2] = {0}, hasil[2][2] = {0}, det;
+    int i, j, k, opsi, pilmatriks;
+    // end deklarasi variabel
 
-    // menentukan perhitungan matriks yang diinginkan
+    // start input value matriks
+    for(i = 0; i < 2; i++) {
+        for(j = 0; j < 2; j++) {
+            cout << "Masukkan Matriks 1 ordo [" << i+1 << "][" << j+1 << "]: ";
+            cin >> matriks1[i][j];
+        }
+    }
+    cout << "\n";
+    for(i = 0; i < 2; i++) {
+        for(j = 0; j < 2; j++) {
+            cout << "Masukkan Matriks 2 ordo [" << i+1 << "][" << j+1 << "]: ";
+            cin >> matriks2[i][j];
+        }
+    }
+    cout << "\n";
+    // end input value matriks
+
+    // start pilih opsi matriks
     cout << "Pilihlah perhitungan matriks 2x2 yang diinginkan\n";
     cout << "1. Penjumlahan\n"; 
     cout << "2. Pengurangan\n"; 
@@ -14,87 +32,81 @@ int main() {
     cout << "4. Determinan Matriks\n"; 
     cout << "5. Invers Matriks\n"; 
     cout << "Masukkan pilihan (contoh: 1): ";
-    cin >> pilih;
+    cin >> opsi;
+    // end pilih opsi matriks
 
     cout << "\n";
 
-    // jika memilih penjumlahan, pengurangan, perkalian
-    if(pilih >=1 && pilih <= 3) {
-        // input value matriks
-        for(i = 0; i < 2; i++) {
-            for(j = 0; j < 2; j++) {
-                cout << "Masukkan Matriks 1 ordo [" << i+1 << "][" << j+1 << "]: ";
-                cin >> matriks1[i][j];
-            }
-        }
-        cout << "\n";
-        for(i = 0; i < 2; i++) {
-            for(j = 0; j < 2; j++) {
-                cout << "Masukkan Matriks 2 ordo [" << i+1 << "][" << j+1 << "]: ";
-                cin >> matriks2[i][j];
-            }
-        }
-
-        // output matriks
+    // start tampilkan value matriks
+    if(opsi >= 1 && opsi <=3) {
         cout << "Matriks 1 = ";
-            for(i = 0; i < 2; i++) {
-                if(i >= 1) {
-                    cout << "\n\t    ";
-                }
-                cout << "| ";
-                for(j = 0; j < 2; j++) {
+        for(i = 0; i < 2; i++) {
+            if(i >= 1) {
+                cout << "\n\t    ";
+            }
+            cout << "| ";
+            for(j = 0; j < 2; j++) {
                     cout << matriks1[i][j] << " ";
             }
             cout << "|";
         }
         cout << "\n\n";
         cout << "Matriks 2 = ";
-            for(i = 0; i < 2; i++) {
-                if(i >= 1) {
-                    cout << "\n\t    ";
-                }
-                cout << "| ";
-                for(j = 0; j < 2; j++) {
-                    cout << matriks2[i][j] << " ";
-            }
-            cout << "|";
-        }
-    } else if(pilih == 4 || pilih == 5) { // jika memilih determinan, invers
-        // input value matriks
         for(i = 0; i < 2; i++) {
-            for(j = 0; j < 2; j++) {
-                cout << "Masukkan Matriks 1 ordo [" << i+1 << "][" << j+1 << "]: ";
-                cin >> matriks1[i][j];
+            if(i >= 1) {
+                cout << "\n\t    ";
             }
+            cout << "| ";
+            for(j = 0; j < 2; j++) {
+                cout << matriks2[i][j] << " ";
+            }
+            cout << "|";
         }
+        cout << "\n\n";
+    } else if(opsi == 4 || opsi == 5) {
+        cout << "Pilih matriks yang diinginkan (1 atau 2): ";
+        cin >> pilmatriks;
+        cout << "\n";
 
-        // output matriks
-        cout << "Matriks 1 = ";
+        if(pilmatriks == 1) {
+            cout << "Matriks = ";
             for(i = 0; i < 2; i++) {
                 if(i >= 1) {
-                    cout << "\n\t    ";
+                    cout << "\n\t  ";
                 }
                 cout << "| ";
                 for(j = 0; j < 2; j++) {
-                    cout << matriks1[i][j] << " ";
+                        cout << matriks1[i][j] << " ";
+                }
+                cout << "|";
             }
-            cout << "|";
+            cout << "\n\n";
+        } else if(pilmatriks == 2) {
+            cout << "Matriks = ";
+            for(i = 0; i < 2; i++) {
+                if(i >= 1) {
+                    cout << "\n\t  ";
+                }
+                cout << "| ";
+                for(j = 0; j < 2; j++) {
+                        cout << matriks2[i][j] << " ";
+                }
+                cout << "|";
+            }
+            cout << "\n\n";
         }
     }
+    // end tampilkan value matriks
 
-    cout << "\n\n";
-
-    // proses menghitung sesuai perhitungan yang diinginkan
-    switch (pilih) {
+    // start proses perhitunan
+    switch (opsi) {
         case 1: // penjumlahan
-            // hitung hasil
             for(i = 0; i < 2; i++) {
                 for(j = 0; j < 2; j++) {
                     hasil[i][j] = matriks1[i][j] + matriks2[i][j];
                 }
             }
 
-            // output
             cout << "Hasil = ";
             for(i = 0; i < 2; i++) {
                 if(i >= 1) {
@@ -107,15 +119,14 @@ int main() {
                 cout << "|";
             }
             break;
+
         case 2: // pengurangan
-            // hitung hasil
             for(i = 0; i < 2; i++) {
                 for(j = 0; j < 2; j++) {
                     hasil[i][j] = matriks1[i][j] - matriks2[i][j];
                 }
             }
 
-            // output
             cout << "Hasil = ";
             for(i = 0; i < 2; i++) {
                 if(i >= 1) {
@@ -128,8 +139,8 @@ int main() {
                 cout << "|";
             }
             break;
+
         case 3: // perkalian
-            // hitung hasil
             for(i = 0; i < 2; i++) {
                 for(j = 0; j < 2; j++) {
                     for(k = 0; k < 2; k++) {
@@ -138,7 +149,6 @@ int main() {
                 }
             }
 
-            // output
             cout << "Hasil = ";
             for(i = 0; i < 2; i++) {
                 if(i >= 1) {
@@ -151,30 +161,51 @@ int main() {
                 cout << "|";
             }
             break;
+
         case 4: // determinan
-            // hitung hasil
-            det = matriks1[0][0] * matriks1[1][1] - matriks1[0][1] * matriks1[1][0];
-            
-            // output
-            cout << "Determinan = " << det;
+            if(pilmatriks == 1) {
+                det = matriks1[0][0] * matriks1[1][1] - matriks1[0][1] * matriks1[1][0];
+                cout << "Determinan = " << det;
+            } else if(pilmatriks == 2) {
+                det = matriks2[0][0] * matriks2[1][1] - matriks2[0][1] * matriks2[1][0];
+                cout << "Determinan = " << det;
+            } else {
+                cout << "Masukkan pilihan matriks 1 atau 2!\n";
+            }
             break;
-            case 5: // invers
-            // hitung determinan
-            det = matriks1[0][0] * matriks1[1][1] - matriks1[0][1] * matriks1[1][0]; 
 
-            // ubah matriks
-            matriks2[0][0] = matriks1[1][1];
-            matriks2[0][1] = matriks1[0][1] * -1;
-            matriks2[1][0] = matriks1[1][0] * -1;
-            matriks2[1][1] = matriks1[0][0];
+        case 5: // invers
+            if(pilmatriks == 1) {
+                det = matriks1[0][0] * matriks1[1][1] - matriks1[0][1] * matriks1[1][0];
+                if (det == 0) {
+                    cout << "Matriks tidak memiliki invers karena determinan = 0.\n";
+                    break;
+                }
+                invers[0][0] = matriks1[1][1];
+                invers[0][1] = -matriks1[0][1];
+                invers[1][0] = -matriks1[1][0];
+                invers[1][1] = matriks1[0][0];
+            } else if(pilmatriks == 2) {
+                det = matriks2[0][0] * matriks2[1][1] - matriks2[0][1] * matriks2[1][0];
+                if (det == 0) {
+                    cout << "Matriks tidak memiliki invers karena determinan = 0.\n";
+                    break;
+                }
+                invers[0][0] = matriks2[1][1];
+                invers[0][1] = matriks2[0][1] * -1;
+                invers[1][0] = matriks2[1][0] * -1;
+                invers[1][1] = matriks2[0][0];
+            } else {
+                cout << "Masukkan pilihan matriks 1 atau 2!\n";
+                break;
+            }
 
             for(i = 0; i < 2; i++) {
                 for(j = 0; j < 2; j++) {
-                    hasil[i][j] = 1 / det * matriks2[i][j];
+                    hasil[i][j] = (1.0 / det) * invers[i][j];
                 }
             }
 
-            // output
             cout << "Hasil = ";
             for(i = 0; i < 2; i++) {
                 if(i >= 1) {
@@ -187,10 +218,12 @@ int main() {
                 cout << "|";
             }
             break;
+
         default:
             cout << "Harap masukkan angka 1 - 5!";
             break;
     }
+    // end proses perhitungan
     
     return 0;
 }
